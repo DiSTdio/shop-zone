4941579dbf37a16d4967b16dacba0e346e2d1194
DiST <happy.scientist.of.my.mind@gmail.com>
2025-05-05 15:22:27 +0900

    for trigger actions

--- shop-zone-react/src/config.js
+++ shop-zone-react/src/config.js
@@ -1,2 +1,1 @@
 export const API_URL = "https://fakestoreapi.in/api/products";
-// export const SVG_URL = "https://cdn-uicons.flaticon.com/2.6.0/uicons-regular-rounded/css/uicons-regular-rounded.css"
